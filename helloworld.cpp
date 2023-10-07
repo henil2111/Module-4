73713170584d6f819077dbd4d38abce6d4627d8e
henil2111 <142875760+henil2111@users.noreply.github.com>
2023-10-07 12:10:52 +0530

    Add files via upload

---
+++ helloworld.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+class alphabet{
+
+
+    public:
+    void word()
+	{
+    	int a;
+    	cout<<"-------Hello World-------"<<endl;
+	}
+};
+int main()
+{
+	alphabet s1;
+	s1.word();
+}
